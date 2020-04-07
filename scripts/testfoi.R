a645457ce139a6652295e8359c62a665ba2a51e6
slwu89 <slwu89@berkeley.edu>
2020-04-07 14:20:31 -0700

    confirm inhom PP sampling works

--- scripts/testfoi.R
+++ scripts/testfoi.R
@@ -1,6 +1,10 @@
 rm(list=ls());gc()
 library(parallel)
 
+# --------------------------------------------------------------------------------
+#   test rejection for constant FOI
+# --------------------------------------------------------------------------------
+
 simmer <- function(tend,rate,dt){
   tnow <- tnext <- 0
   nreject <- 0
@@ -19,55 +23,63 @@ simmer <- function(tend,rate,dt){
 # debug(simmer)
 # simmer(tend = 1e4,rate = 1/20,dt = 1)
 
-n <- 2e6
-lambda <- 1/20
-dt <- 0.5
-
-out_05 <- mclapply(X = 1:n,FUN = function(i){simmer(tend = 1e4,rate = lambda,dt = dt)})
-out_05 <- do.call(rbind,out_05)
-
-out_1 <- mclapply(X = 1:n,FUN = function(i){simmer(tend = 1e4,rate = lambda,dt = 1)})
-out_1 <- do.call(rbind,out_1)
+# n <- 2e6
+# lambda <- 1/20
+# dt <- 0.5
+#
+# out_05 <- mclapply(X = 1:n,FUN = function(i){simmer(tend = 1e4,rate = lambda,dt = dt)})
+# out_05 <- do.call(rbind,out_05)
+#
+# out_1 <- mclapply(X = 1:n,FUN = function(i){simmer(tend = 1e4,rate = lambda,dt = 1)})
+# out_1 <- do.call(rbind,out_1)
 # out <- replicate(1e5,simmer(tend = n,rate = 1/20,dt = 1))
 
 # colMeans(out_05)
-# 
+#
 # p_dt <- 1-exp(-lambda*dt)
 # 1/p_dt
 # mean(rgeom(n = n,p_dt)+1)
-# 
+#
 # (1/p_dt)*0.5
-# 
+#
 # (1/lambda) + (dt / (1-exp(lambda*dt)))
-# 
-# 
+#
+#
 # b = 1.5
 # lambda <- 1/2
 # mean(rtrunc(n = 4e6,spec = "exp",b = b,rate = lambda))
-# 
+#
 # (1/lambda) + (b / (1-exp(lambda*b)))
 
 
-# NHPP: piecewise constant intensity
 
-findInterval()
+# --------------------------------------------------------------------------------
+# NHPP: piecewise constant intensity
+# --------------------------------------------------------------------------------
+
+# findInterval()
 
 x <- seq(0,48,by=0.01)
-cyclicIntensity <- function(t){
+cyclicIntensity <- function(t,fudge=T){
   # t <- t %% 24
-  (1/8)*sin((t - 6)*2*pi/24)+((1/8)+.Machine$double.eps)
+  if(fudge){
+    (1/8)*sin((t - 6)*2*pi/24)+((1/8)+.Machine$double.eps)
+  } else {
+    (1/8)*sin((t - 6)*2*pi/24)+(1/8)
+  }
 }
 
 cyclicApprox <- approx(x = 0:23,y = cyclicIntensity(0:23),method = "constant",n=length(0:23))
 
+# Intensity
 plot(x,cyclicIntensity(x),type="l",ylim = c(0,0.25),
      main = "Inhomogeneous Intensity Function",xaxt="n",
      xlab = "Time (hours)",ylab = "Intensity",col="firebrick3",lwd=1.5)
 
 for(i in 0:48){
-  
+
   j <- (i %% 24) + 1
-  
+
   segments(x0 = i,x1 = i+1,
            y0 = cyclicApprox$y[j],y1 = cyclicApprox$y[j],
            col = "darkorchid3",lwd=1.5)
@@ -75,14 +87,41 @@ for(i in 0:48){
   points(x=i+1,y=cyclicApprox$y[j],pch=1,col="darkorchid3",cex=0.5)
   if(i != 48){
     segments(x0 = i+1,x1 = i+1,y0 = cyclicApprox$y[j],y1 = cyclicApprox$y[j+1],
-             lty = 2,col="darkorchid3") 
+             lty = 2,col="darkorchid3")
   }
-  
+
 }
 axis(side = 1,at = c(0,cumsum(rep(6,48/6))),
      labels = c(0,as.character(rep(cumsum(rep(6,24/6)),times=2))))
 
 
+# time to 1st event
+lambda <- cyclicIntensity(t = x,fudge = FALSE)
+lambda_discrete <- approx(x = x,y = cyclicIntensity(x,F),method = "constant",n = length(x))
+
+IntegratedCyclicIntensity <- function(t){
+  (1/8) * (t - ( (12 * sin( (pi*t) / 12 ) ) / pi ))
+}
+
+Lambda <- IntegratedCyclicIntensity(t = x)
+Lambda_discrete <- cumsum(lambda_discrete$y*0.01)
+
+pdf_1st <- lambda * exp(-Lambda)
+pmf_1st <- lambda_discrete$y * exp(-Lambda_discrete)
+
+plot(x,pdf_1st,type="l",
+     xlab = "Time (hours)",ylab = "Density",
+     main = "Time to First Event",col = "firebrick3",lwd=1.5)
+# lines(x,pmf_1st,col="darkorchid3",lwd=1.5,lty=1)
+
+
+
+
+# --------------------------------------------------------------------------------
+# NHPP: 1st event time in R by inversion
+# --------------------------------------------------------------------------------
+
+# for 14 days
 piecewise_approx <- approx(x = 0:(24*14),y = cyclicIntensity(0:(24*14)),method = "constant",n=length(0:(24*14)))
 
 NHPP_inversion_1st <- function(approx){
@@ -110,7 +149,7 @@ times <- unlist(parallel::mclapply(X = 1:1e5,FUN = function(x){NHPP_inversion_1s
 hist_out <- hist(times[times<48],breaks = 48)
 
 # NHPP_rejection <- function(){
-#   
+#
 # }
 
 library(stocheulerABM)
@@ -125,31 +164,43 @@ out <- stocheulerABM::inhomPP_piecewiseconst(
 times2 <- unlist(parallel::mclapply(X = 1:1e5,FUN = function(x){stocheulerABM::inhomPP_piecewiseconst(
   tvec = piecewise_approx$x,
   lambdavec = piecewise_approx$y,
-  tmax = tail(piecewise_approx$y,1),
+  tmax = tail(piecewise_approx$x,1),
   first = T
 )}))
 
 hist(times2[times2<48],breaks = 48)
+
+times_lambda <- unlist(parallel::mclapply(X = 1:1e5,FUN = function(x){stocheulerABM::inhomPP_piecewiseconst(
+  tvec = lambda_discrete$x,
+  lambdavec = lambda_discrete$y,
+  tmax = tail(lambda_discrete$x,1),
+  first = T
+)}))
+
+plot(x,pdf_1st,type="l",
+     xlab = "Time (hours)",ylab = "Density",
+     main = "Time to First Event",col = "firebrick3",lwd=2.15)
+hist(times_lambda,breaks=48,probability = T,add=T,col = adjustcolor("steelblue",alpha.f = 0.5),border=grey(0.5,0.5))
 
 # NHPP_inversion_all <- function(approx,tmax){
 #   a <- 0
 #   u <- 0
 #   j <- 1
 #   Lambda <- c(0,0)
-#   
+#
 #   t <- approx$x
 #   lambda <- approx$y
-#   
+#
 #   while(tail(a,1) < tmax){
-#     
+#
 #     u <- u + rexp(n=1)
 #     Lambda[j] <- 0 # lambda_{j}
 #     Lambda[j+1] <- Lambda[1] + 0.5*(lambda[j+1] + lambda[j])*(t[j+1] - t[j]) # lambda_{j+1}
-#     
-#     
+#
+#
 #   }
-#   
-#   
+#
+#
 #   t <- approx$x
 #   lambda <- approx$y
 #   j <- 1
