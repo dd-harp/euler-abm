9bc3b37878d1eee62eb88eca736d60359af2d9ca
slwu89 <slwu89@berkeley.edu>
2020-09-27 12:59:43 -0700

    scripts to run lattice of delta t

--- figs/scripts/fig_deltalatticeNonMarkov.R
+++ figs/scripts/fig_deltalatticeNonMarkov.R
@@ -1,6 +1,6 @@
 # --------------------------------------------------------------------------------
 #
-#   Figure: evaluate Markov SIR ABM over lattice of delta t
+#   Figure: evaluate non-Markov SIR ABM over lattice of delta t
 #   Sean L. Wu (slwu89@berkeley.edu)
 #   September 2020
 #
@@ -29,6 +29,15 @@ N <- S0 + I0
 R0 <- 2.5
 gamma <- 1/3.5
 beta <- R0 * (gamma/N)
+
+# non-Markov parameters
+gamma_mean <- 1/gamma
+gamma_var <- 0.5^2
+
+gamma_shape <- (gamma_mean^2)/gamma_var
+gamma_scale <- gamma_var/gamma_mean
+
+# stop time
 tmax <- 5
 
 # mc reps
@@ -47,25 +56,26 @@ cl <- parallel::makeCluster(ncores)
 doParallel::registerDoParallel(cl)
 
 # use parallel RNG for multiple streams of random numbers
-parallel::clusterSetRNGStream(cl = cl,iseed = 186337428L)
+parallel::clusterSetRNGStream(cl = cl,iseed = 257152385L)
 
 # parallel options
 opts <- list(preschedule=FALSE)
 
 mc_reps <- expand.grid(rep = 1:nrep,dt = abm_dt)
 
-deltaMarkov_ABM <- foreach(i = mc_reps$rep, dt = mc_reps$dt,.combine = "rbind",.options.multicore=opts,.packages = c("stocheulerABM")) %dopar% {
+deltaNonMarkov_ABM <- foreach(i = mc_reps$rep, dt = mc_reps$dt,.combine = "rbind",.options.multicore=opts,.packages = c("stocheulerABM")) %dopar% {
 
-   out <- stocheulerABM::SIRMarkov_ABM(
-     tmax = tmax,
-     dt = dt,
-     S = S0,
-     I = I0,
-     R = 0,
-     beta = beta,
-     gamma = gamma,
-     verbose = FALSE
-   )
+  out <- stocheulerABM::SIRnonMarkov_ABM(
+    tmax = tmax,
+    dt = dt_ABM,
+    S = S0,
+    I = I0,
+    R = 0,
+    beta = beta,
+    gamma_shape = gamma_shape,
+    gamma_scale = gamma_scale,
+    verbose = FALSE
+  )
 
     c("dt" = dt,"S"=tail(out$S,1),"I"=tail(out$I,1))
 }
@@ -74,4 +84,4 @@ deltaMarkov_ABM <- foreach(i = mc_reps$rep, dt = mc_reps$dt,.combine = "rbind",.
 close(pb)
 parallel::stopCluster(cl);rm(cl);gc()
 
-saveRDS(object = deltaMarkov_ABM,file = here::here("/figs/deltaMarkov_ABM.rds"),compress = TRUE)
+saveRDS(object = deltaNonMarkov_ABM,file = here::here("/figs/deltaNonMarkov_ABM.rds"),compress = TRUE)
